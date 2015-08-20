a6faf56f53c8f14bec0331375768f042d3baba78
Rasmus Praestholm <cervator@gmail.com>
2015-08-19 22:44:18 -0400

    Content tutorial step 2 - http://docs.alfresco.com/5.0/tasks/dev-extensions-content-models-tutorials-create-custom-content.html

---
+++ repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/modeltest.get.js
@@ -0,0 +1,12 @@
+var contentType = "my:whitepaper";
+var documentName = url.templateArgs.documentName;
+
+var document = companyhome.createNode(documentName, contentType);
+
+if (document != null){
+    model.document = document;
+    model.msg = "Created OK!";
+}
+else {
+    model.msg = "Failed to create document!";
+}
