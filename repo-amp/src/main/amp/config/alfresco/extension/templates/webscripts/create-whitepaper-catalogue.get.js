8e23a07a664a01632218c3e69b4d32cabfb2abe5
Rasmus Praestholm <cervator@gmail.com>
2015-08-24 17:01:21 -0400

    Create a new content type for whitepaper catalogues + a matching webscript to create.
    - Note how the new object is at the same level of a whitepaper, but doesn't get the customization from Share.
    - Whitepapers can be created with links like http://localhost:8080/alfresco/service/modeltest/MyWhitepaper123
    - While catalogues can be created with links like http://localhost:8080/alfresco/service/create-whitepaper-catalogue/MyCatalogue123
    - Discovery: Webscripts CANNOT have periods in the main part of the name, thus the hyphens ;-)

--- repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/create-whitepaper-catalogue.get.js
+++ repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/create-whitepaper-catalogue.get.js
@@ -1,11 +1,11 @@
-var contentType = "myc:whitepaper";
+var contentType = "myc:whitepaperCatalogue";
 var documentName = url.templateArgs.documentName;
 
 var document = companyhome.createNode(documentName, contentType);
 
 if (document != null){
     model.document = document;
-    model.msg = "Created whitepaper OK!";
+    model.msg = "Created whitepaper catalogue OK!";
 }
 else {
     model.msg = "Failed to create document!";
